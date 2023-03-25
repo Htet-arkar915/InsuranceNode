44651ec185b9b9319a35a028f4a9b873332f856d
Htet Arkar Linn <htetarkar915@gmail.com>
2023-03-25 13:52:07 +0630

    gettopost

--- routes/api/insurance.js
+++ routes/api/insurance.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const insuranceController = require("../../controllers/insuranceController");
 
 router
-  .get("/", insuranceController.getInsuranceResult)
+  .post("/", insuranceController.getInsuranceResult)
   .get("/alldata", insuranceController.getAllInsurance)
   .post("/", insuranceController.createInsurance);
 module.exports = router;
