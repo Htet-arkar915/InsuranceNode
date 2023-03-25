a8e3dc415975d1f1dd8b6216cb5d44c6fbd90a35
Htet Arkar Linn <htetarkar915@gmail.com>
2023-03-25 16:24:03 +0630

    email

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -83,43 +83,43 @@ const sendMail = async (req, res) => {
     if (!req?.body?.mail) {
       return res.status(401).json({ message: "User email is required" });
     } else {
-      pdf(document, options)
-        .then((result) => {
-          console.log(result);
-          const sendPdf = fs.readFileSync("./file/output.pdf");
-          const mailOptions = {
-            from: `"YIG INSURANCE" <${process.env.MAIL}>`,
-            to: req.body.mail,
-            subject: "Mail For Our Policy",
-            text: "Please see the attached PDF file",
-            html:
-              "<h4>Dear [Customer],</h4>" +
-              "<p>I hope this email finds you well. I wanted to take a moment to express my sincerest thanks for choosing Young Insurance for your travel insurance needs. We are thrilled to have you as a customer and we appreciate your business.</p> " +
-              "<p>Attached to this email, you will find the travel insurance policy PDF, which outlines the terms and conditions of your coverage. We encourage you to review it carefully and contact us if you have any questions or concerns.</p>" +
-              "<p>We understand that travel can be unpredictable, and having travel insurance can provide peace of mind while you are on your trip. Our team is committed to ensuring that you have a positive experience with our products and services.</p>" +
-              "<p>Thank you again for choosing Young Insurance. We value your trust in us and look forward to serving you in the future.</p>" +
-              "<h4>Best regards,</h4>" +
-              "<h3>Young Insurance</h3>",
-            attachments: [
-              {
-                filename: "output.pdf",
-                content: sendPdf,
-              },
-            ],
-          };
-          smtpProtocol.sendMail(mailOptions, (err, response) => {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("Mail sent" + response.message);
-              smtpProtocol.close();
-            }
-          });
-          res.status(201).json({ message: `Mail sent to ${req.body.mail}` });
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      //pdf(document, options)
+      //  .then((result) => {
+      //    console.log(result);
+      const sendPdf = fs.readFileSync("./file/insurance.pdf");
+      const mailOptions = {
+        from: `"YIG INSURANCE" <${process.env.MAIL}>`,
+        to: req.body.mail,
+        subject: "Mail For Our Policy",
+        text: "Please see the attached PDF file",
+        html:
+          "<h4>Dear [Customer],</h4>" +
+          "<p>I hope this email finds you well. I wanted to take a moment to express my sincerest thanks for choosing Young Insurance for your travel insurance needs. We are thrilled to have you as a customer and we appreciate your business.</p> " +
+          "<p>Attached to this email, you will find the travel insurance policy PDF, which outlines the terms and conditions of your coverage. We encourage you to review it carefully and contact us if you have any questions or concerns.</p>" +
+          "<p>We understand that travel can be unpredictable, and having travel insurance can provide peace of mind while you are on your trip. Our team is committed to ensuring that you have a positive experience with our products and services.</p>" +
+          "<p>Thank you again for choosing Young Insurance. We value your trust in us and look forward to serving you in the future.</p>" +
+          "<h4>Best regards,</h4>" +
+          "<h3>Young Insurance</h3>",
+        attachments: [
+          {
+            filename: "insurance.pdf",
+            content: sendPdf,
+          },
+        ],
+      };
+      smtpProtocol.sendMail(mailOptions, (err, response) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Mail sent" + response.message);
+          smtpProtocol.close();
+        }
+      });
+      res.status(201).json({ message: `Mail sent to ${req.body.mail}` });
+      //})
+      //.catch((error) => {
+      //  console.error(error);
+      //});
     }
   } catch (e) {
     console.log(e);
